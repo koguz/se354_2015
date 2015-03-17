1ebe0a937c5f3cafa704ff61776615c1639a0648
Kaya Oğuz <kayaoguz@gmail.com>
2015-03-17 23:40:57 +0200

    Automata map generator update
    I mark the regions and as I'm marking I also compute each of the
    regions' area. The region with the largest area remains, others become
    walls. At the same time, if the region area is less than half the map's
    area, I invert the map, that is, walls become floors, floors become
    walls and I get the larger area as the floors. Check the comments as
    well.

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -45,6 +45,8 @@ public class DungeonGenerator : MonoBehaviour {
 			}
 		}
 		// temp holds the final map. 
+		// create a 102x102 map and copy 100x100 into this, so that
+		// the map will be surrounded by walls. 
 		map = new int[102, 102];
 		for(int i=0;i<102;i++) {
 			for(int j=0;j<102;j++) {
@@ -56,7 +58,64 @@ public class DungeonGenerator : MonoBehaviour {
 				map[i,j] = temp[i-1,j-1];
 			}
 		}
+
+		// post processing
+		// here's the deal: mark every region and find out the largest one. 
+		// make all of the smaller ones walls. 
+
+		// how to mark every region? start from the beginning and when you
+		// come accross a floor tile, start bucket filling. 
+		int reg = 2; // begin from ID=2 - 0 is floor, 1 is wall
+		int maxArea = 0;
+		int maxRegn = 2;
+		for(int i=1;i<101;i++) {
+			for(int j=1;j<101;j++) {
+				if(map[i,j] == 0) {
+					// calculate the area as you bucket fill 
+					int area = bucketFill(i,j,reg);
+					if(area > maxArea) {
+						maxArea = area;
+						maxRegn = reg;
+					}
+					reg++; // increment for the next region. 
+				}
+			}
+		}
+
+		// here is another trick
+		// if the maxArea is less than 5000 tiles, 
+		// that means more than half of the region 
+		// is walls. we want floors, so i switch the
+		// map's floor and wall id's if the maxarea is
+		// not big enough. 
+		int floorid = 0; 
+		int wallid  = 1;
+		if(maxArea < 5000) { // 100x100 ) 10.000
+			floorid = 1; wallid = 0;
+		}
+
+		// Regions are marked, the max area is saved in maxRegn.
+		// Go once more through the tiles, remove the smaller regions. 
+		for(int i=1;i<101;i++) {
+			for(int j=1;j<101;j++) {
+				// if tile is max region, make it 1, else if it is not 0 (floor), then
+				// it is another region. make it a floor (0). 
+				if(map[i,j] == maxRegn) map[i,j] = floorid;
+				else if(map[i,j] != 0) map[i,j] = wallid;
+			}
+		}
+
 		drawMap();
+	}
+
+	private int bucketFill(int x, int y, int r) {
+		int toplam = 1; 
+		map[x,y] = r;
+		if(map[x-1,y] == 0) toplam += bucketFill (x-1, y, r);
+		if(map[x+1,y] == 0) toplam += bucketFill (x+1, y, r);
+		if(map[x,y-1] == 0) toplam += bucketFill (x, y-1, r);
+		if(map[x,y+1] == 0) toplam += bucketFill (x, y+1, r);
+		return toplam;
 	}
 
 	private void drawMap() {
